cfc51f491321e66e5f57c52835a2b46c512a42b8
Austin Durham <durhamaustin9@gmail.com>
2019-02-11 17:18:15 -0700

    Updated school page

--- Javascript/school.js
+++ Javascript/school.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  var color 
+  var color
   
   $( ".tablinks" ).click(function() {
     
-    $( ".tablinks" ).css("background-color","#ffffff"); //Unselected tab background color
+    $( ".tablinks" ).css("background-color","transparent"); //Unselected tab background color
     var openthis = $(this).attr('id');  //id atribute selector
 
-    $(this).css("background-color","#cccccc");  //Selected tab background color
+    $(this).css("background-color","#055ea3");  //Selected tab background color
     $(".tabcontent").css("display","none");
     $(".tabcontent#" + openthis).fadeIn("slow");
   }); //Controls tab buttons
@@ -19,15 +19,21 @@ $(document).ready(function() {
     var selector = $(this).attr('id');
     
     if ($(".tabcontent#" + selector).css("display") != ("block"))  {
-      $(this).css("background-color","#cccccc");
+      $(this).css("background-color","#055ea3");
     }
   }); //Controls mouse hover affect
   
   $( ".tablinks" ).mouseleave(function() {
     var selector = $(this).attr('id');
     
-    if ($(".tabcontent#" + selector).css("display") != ("block"))  { 
-      $(this).css("background-color","#ffffff");
-    }      
+    if ($(".tabcontent#" + selector).css("display") != ("block"))  {
+      $(this).css("background-color","transparent");
+    }
   }); //add on to the mouse hover affect
+
+  $(".HSsynopsis").ready(function() {
+    if ($(document).css("width") >= ("955px"))  {
+      
+    }
+  });
 });
