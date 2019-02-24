80dbbe2406d0be70268fdb20d8ac32f7bef2b0cd
durhamaustin9 <durhamaustin9@gmail.com>
2019-02-24 00:54:25 -0700

    minor homepage js update

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,30 +1,19 @@
 $(document).ready(function()  {
-  
+
   $(window).scroll(function() {
     var scroll = $(this).scrollTop();
-    
-    if(scroll >= 400) {
-      document.getElementById("innerNavbar").style.position = "fixed";
-      document.getElementById("innerNavbar").style.marginTop = 0;
-    }
-    
-    if(scroll < 400)  {
+
+    if  (scroll < 400)  {
+
       document.getElementById("innerNavbar").style.position = "absolute";
-      document.getElementById("innerNavbar").style.marginTop = 400;
-      
+      document.getElementById("innerNavbar").style.marginTop = "400px";
     }
-    
-//    if(scroll <= 400) {
-//      $("Navbar").ready(function()  {
-//        var margin = document.getElementById("Navbar").css("margin-Top"); 
-//        
-//        for (document.getElementById("Navbar").css("margin-Top") <= 400;)  {
-//          $("Navbar").getElementById.css("marginTop") - $(document).scrollTop();
-//        }
-//      });
-//      
-//      document.getElementById("Navbar").marginTop = ()
-//    }
+
+    if  (scroll >= 400)  {
+      document.getElementById("innerNavbar").style.position = "fixed";
+      document.getElementById("innerNavbar").style.marginTop = "0";
+
+    }
   });
   
 });
