b0d5cfb940954ac7c1d7e3e61b5c9c84b423bfa6
Austin Durham <durhamaustin9@gmail.com>
2019-05-31 22:31:12 -0700

    New design on school page

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,15 +1,61 @@
 $(document).ready(function()  {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
 
+    $('html').resize(function(){
+
+        var width = window.innerWidth;
+        var height = window.innerHeight;
+
+        if (width >= 768) {
+            this.close();
+        }
+    });
 });
 
-function compressNav(x) {
-  x.classList.toggle("change");
+var delay = 500;
 
-  // if ($(".navbariteam").css("display") === "none")  {
-  //   $(".navbariteam").css("display", "table-row")
-  // }
-  //
-  // else {
-  //   $(".navbariteam").css("display", "none")
-  // }
+function compressNav(x) {
+    x.classList.toggle("change");
+
+    if ($(".Navbar").css("height") === "100px") {
+        this.open();
+    } else {
+        this.close();
+    }
+}
+
+function close() {
+    $(".Navbar").css("height", "100");
+
+    $(".ul").css("height", "100px");
+
+    $(".navbar-underly").animate({
+        height: "100px",
+        opacity: ".8"
+    }, {
+        duration: "2000"
+    });
+
+    $(".navbaritem").fadeOut("fast");
+
+    setTimeout(function () {
+        $(".navbaritem").css("display", "");
+        $(".ul").css("height", "")
+    }, delay);
+}
+
+function open() {
+    $(".Navbar").css("height", "342px");
+
+    $(".ul").css("height", "342px");
+
+    $(".navbar-underly").animate({
+        height: "342px",
+        opacity: ".91"
+    }, {
+        duration: "2000"
+    });
+
+    $(".navbaritem").fadeIn("slow")
 }
--- Javascript/school.js
+++ Javascript/school.js
@@ -1,39 +1,13 @@
 $(document).ready(function() {
-  var color
-  
-  $( ".tablinks" ).click(function() {
-    
-    $( ".tablinks" ).css("background-color","transparent"); //Unselected tab background color
-    var openthis = $(this).attr('id');  //id atribute selector
-
-    $(this).css("background-color","#055ea3");  //Selected tab background color
-    $(".tabcontent").css("display","none");
-    $(".tabcontent#" + openthis).fadeIn("slow");
-  }); //Controls tab buttons
-
-  $( ".tablinks" ).ready(function() {
-    $("#freshmen").click();
-  }); //Displays freshmen tab automatically
-  
-  $( ".tablinks" ).hover(function()  {
-    var selector = $(this).attr('id');
-    
-    if ($(".tabcontent#" + selector).css("display") != ("block"))  {
-      $(this).css("background-color","#055ea3");
-    }
-  }); //Controls mouse hover affect
-  
-  $( ".tablinks" ).mouseleave(function() {
-    var selector = $(this).attr('id');
-    
-    if ($(".tabcontent#" + selector).css("display") != ("block"))  {
-      $(this).css("background-color","transparent");
-    }
-  }); //add on to the mouse hover affect
-
-  $(".HSsynopsis").ready(function() {
-    if ($(document).css("width") >= ("955px"))  {
-      
-    }
+  $('.centerSplit').animate({
+    opacity: '.5'
+  }, {
+    duration: 3000
   });
+
+  $('.underline').animate({
+    opacity: '1'
+  },  {
+    duration: 3000
+  })
 });
