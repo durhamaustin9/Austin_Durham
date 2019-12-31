93aaa03abf546d672f967dfd0735e642d71d6845
austindurham9 <durhamaustin@gmail.com>
2019-12-31 13:56:08 -0700

    Started support for mobil systems on school page, along with style change.

--- Javascript/school.js
+++ Javascript/school.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
+  let gpaValue = 0;
+
+  let Animation = setInterval(gpaAnimate, 10);
+
+  function gpaAnimate() {
+    document.getElementById('progressAnimate').style.width = gpaValue + '%';
+
+    gpaValue = gpaValue + .5;
+
+    let gpa = document.getElementById('progressAnimate').style.width;
+
+    if (parseInt(gpa) >= 67.5) {
+      clearInterval(Animation);
+
+      $('.progress-bar-number').fadeIn("slow");
+    }
+  }
+
   $('.centerSplit').animate({
     opacity: '.5'
   }, {
